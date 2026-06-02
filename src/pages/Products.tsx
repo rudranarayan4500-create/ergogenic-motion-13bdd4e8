4e317b0aff0dd0e1f0b85d24b5c522efc9f248b0
rudranarayan4500-create <rudranarayan4500@gmail.com>
2026-06-03 01:18:25 +0530

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -108,7 +108,7 @@ const Products = () => {
           mrp: 1599,
           category: "Performance" as Category,
           tagline: "200-mesh pure micronized athletic phosphagen compound built to maximize systemic muscular cell hydration thresholds.",
-          image: "https://rjsmqpneamauasuoqzct.supabase.co/storage/v1/object/public/review-photos//WhatsApp%20Image%202026-05-31%20at%207.38.06%20PM.jpeg"
+          image: "https://rjsmqpneamauasuoqzct.supabase.co/storage/v1/object/public/review-photos//596a34ab-1f16-48d9-ba17-e78d005ec14c.png"
         };
       }
       return p;
